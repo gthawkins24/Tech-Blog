e4385f1bac0ff1d88722e6cb1601cb4cdeb0222c
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-03-13 19:46:55 -0400

    fixed bug where couldn't logout from dashboard page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,16 +3,18 @@ const router = require('express').Router();
 const userAPI = require('../controllers/api/user.js');
 const postAPI = require('../controllers/api/post.js');
 
+const withAuth = require('../utils/auth.js');
+
 // user routes
 router.get('/api/users', userAPI.getAllUsers);
-router.post('/api/users', userAPI.createNewUser);
-router.post('/api/users/login', userAPI.userLogin);
-router.post('/api/users/logout', userAPI.userLogout);
+router.get('/api/posts', postAPI.getAllPosts);
 router.get('/api/users/:id', userAPI.getOneUser);
+router.get('/api/posts/:id', postAPI.getOnePost);
 
 // post routes
-router.get('/api/posts', postAPI.getAllPosts);
+router.post('/api/users', userAPI.createNewUser);
 router.post('/api/posts', postAPI.createPost);
-router.get('/api/posts/:id', postAPI.getOnePost);
+router.post('/api/users/login', userAPI.userLogin);
+router.post('/api/users/logout', userAPI.userLogout);
 
 module.exports = router;
