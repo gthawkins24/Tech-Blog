52bf0a5e1d29fd5ce11ef25826dadebe8fd047cc
Gregory Hawkins <91228189+gthawkins24@users.noreply.github.com>
2022-03-13 21:48:30 -0400

    added post list for dashboard

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -101,7 +101,42 @@ exports.logout = (req, res) => {
 }
 
 exports.displayDashboard = (req, res) => {
-  res.render('dashboard', {
-    loggedIn: req.session.loggedIn
-  });
+  console.log(req.session);
+  console.log('======================');
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'post_content',
+      'title',
+      'created_at',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { 
+        posts, 
+        loggedIn: req.session.loggedIn
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 }
